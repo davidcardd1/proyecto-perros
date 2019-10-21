b016eab9963beb2d1f766adfeb2726a44feb738d
davidcardd1 <52186152+davidcardd1@users.noreply.github.com>
2019-10-20 19:50:37 -0500

    Update models.py

--- Proyecto4Patas/Apps/Login/models.py
+++ Proyecto4Patas/Apps/Login/models.py
@@ -2,10 +2,21 @@ from django.db import models
 
 # Create your models here.
 class Usuario (models.Model):
-    nombre = models.CharField(max_length=15)
-    contrasena=models.CharField(null=True,blank=True,max_length=15)
-    user_Name= models.CharField(max_length=15)
-    correo=models.EmailField()
-    descripcion=models.TextField( null=True,blank=True)
-    foto=models.ImageField( null=True,blank=True)
+    
+    user = AutoOneToOneField(settings.AUTH_USER_MODEL, related_name='forum_profile', verbose_name=_('User'))
+    profile_pic = ExtendedImageField(_('Profile pic'), blank=True, default='')
+    post_count = models.IntegerField(_('Post count'), blank=True, default=0)
+    email = models.EmailField()
+    description=models.TextField( null=True,blank=True)
+
+    class Meta:
+        verbose_name = _('Profile')
+        verbose_name_plural = _('Profiles')
+
+    def last_post(self):
+        posts = Post.objects.filter(user__id=self.user_id).order_by('-created')
+        if posts:
+            return posts[0].created
+        else:
+            return None
     
