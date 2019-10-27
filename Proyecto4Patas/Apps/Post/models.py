1953a1a89a59c160bb8250cc54b6cf0132c91637
davidcardd1 <52186152+davidcardd1@users.noreply.github.com>
2019-10-27 09:49:03 -0600

    base funcional

--- Proyecto4Patas/Apps/Post/models.py
+++ Proyecto4Patas/Apps/Post/models.py
@@ -1,93 +1,20 @@
 from django.db import models
 from Apps.Login.models import Usuario
+from django.conf import settings
 
-# Create your models here.
-
-    
- class Post(models.Model):
-    topic = models.ForeignKey(Topic, related_name='posts', verbose_name=_('Topic'))
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='posts', verbose_name=_('User'))
-    created = models.DateTimeField(_('Created'), auto_now_add=True)
-    updated = models.DateTimeField(_('Updated'), blank=True, null=True)
-    body = models.TextField(_('Message'))
-    body_html = models.TextField(_('HTML version'))
-
-
-    class Meta:
-        ordering = ['created']
-        get_latest_by = 'created'
-        verbose_name = _('Post')
-        verbose_name_plural = _('Posts')
-
-    def save(self, *args, **kwargs):
-        self.body_html = convert_text_to_html(self.body, self.markup)
-        super(Post, self).save(*args, **kwargs)
-
-
-    def delete(self, *args, **kwargs):
-        self_id = self.id
-        head_post_id = self.topic.posts.order_by('created')[0].id
-        forum = self.topic.forum
-        topic = self.topic
-        profile = self.user.forum_profile
-        self.last_topic_post.clear()
-        self.last_forum_post.clear()
-        super(Post, self).delete(*args, **kwargs)
-        #if post was last in topic - remove topic
-        if self_id == head_post_id:
-            topic.delete()
-        else:
-            try:
-                topic.last_post = Post.objects.filter(topic__id=topic.id).latest()
-            except Post.DoesNotExist:
-                topic.last_post = None
-            topic.post_count = Post.objects.filter(topic__id=topic.id).count()
-            topic.save()
-        try:
-            forum.last_post = Post.objects.filter(topic__forum__id=forum.id).latest()
-        except Post.DoesNotExist:
-            forum.last_post = None
-        #TODO: for speedup - save/update only changed fields
-        forum.post_count = Post.objects.filter(topic__forum__id=forum.id).count()
-        forum.topic_count = Topic.objects.filter(forum__id=forum.id).count()
-        forum.save()
-        profile.post_count = Post.objects.filter(user__id=self.user_id).count()
-        profile.save()
-
-    
-class Forum(models.Model):
-    name = models.CharField(_('Name'), max_length=80)
-    position = models.IntegerField(_('Position'), blank=True, default=0)
-    description = models.TextField(_('Description'), blank=True, default='')
-    updated = models.DateTimeField(_('Updated'), auto_now=True)
-    post_count = models.IntegerField(_('Post count'), blank=True, default=0)
-    last_post = models.ForeignKey('Post', related_name='last_forum_post', blank=True, null=True)
-
-    class Meta:
-        ordering = ['position']
-        verbose_name = _('Forum')
-        verbose_name_plural = _('Forums')
-
-    def __str__(self):
-        return self.name
-
-    @property
-    def posts(self):
-        return Post.objects.filter(topic__forum__id=self.id).select_related()
-
-
+# Create your models here.       
 
 class Topic(models.Model):
-    name = models.CharField(_('Subject'), max_length=255)
-    created = models.DateTimeField(_('Created'), auto_now_add=True)
-    updated = models.DateTimeField(_('Updated'), null=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_('User'))
-    views = models.IntegerField(_('Views count'), blank=True, default=0)
-    closed = models.BooleanField(_('Closed'), blank=True, default=False)
-    subscribers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='subscriptions', verbose_name=_('Subscribers'), blank=True)
-    post_count = models.IntegerField(_('Post count'), blank=True, default=0)
-    last_post = models.ForeignKey('Post', related_name='last_topic_post', blank=True, null=True)
-
+    name = models.CharField('Subject', max_length=255)
+    created = models.DateTimeField('Created', auto_now_add=True)
+    updated = models.DateTimeField('Updated', null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='topicUser',on_delete=models.CASCADE)
+    no_views = models.IntegerField('Views count', blank=True, default=0)
+    subscribers = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True)
+    post_count = models.IntegerField('Post count', blank=True, default=0)
+    #last_post = models.ForeignKey('Post', blank=True, null=True)
+    
+    """
     class Meta:
         ordering = ['-updated']
         get_latest_by = 'updated'
@@ -145,4 +72,82 @@ class Topic(models.Model):
             #initialize topic tracking dict
             tracking.topics = {self.id: self.last_post_id}
             tracking.save()
+
+    """
+
+class Forum(models.Model):
+    name = models.CharField('Name', max_length=80)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, default='0')
+    position = models.IntegerField('Position', blank=True, default=0)
+    description = models.TextField('Description', blank=True, default='')
+    updated = models.DateTimeField('Updated', auto_now=True)
+    post_count = models.IntegerField('Post count', blank=True, default=0)
+    #last_post = models.ForeignKey('Post', related_name='last_forum_post', blank=True, null=True)
+
+    """
+    class Meta:
+        ordering = ['position']
+        verbose_name = 'Forum'
+        verbose_name_plural = 'Forums'
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def posts(self):
+        return Post.objects.filter(topic__forum__id=self.id).select_related()
+    """
+
+class Post(models.Model):
+    foro = models.ForeignKey(Forum, on_delete=models.CASCADE, null=False, default='0')
+    users = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created = models.DateTimeField('Created', auto_now_add=True)
+    updated = models.DateTimeField('Updated', blank=True, null=True)
+    body = models.TextField('Message')
+    body_html = models.TextField('HTML version')
+
+
+    """
+    class Meta:
+        ordering = ['created']
+        get_latest_by = 'created'
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+    def save(self, *args, **kwargs):
+        self.body_html = convert_text_to_html(self.body, self.markup)
+        super(Post, self).save(*args, **kwargs)
+
+
+    def delete(self, *args, **kwargs):
+        self_id = self.id
+        head_post_id = self.topic.posts.order_by('created')[0].id
+        forum = self.topic.forum
+        topic = self.topic
+        profile = self.user.forum_profile
+        self.last_topic_post.clear()
+        self.last_forum_post.clear()
+        super(Post, self).delete(*args, **kwargs)
+        #if post was last in topic - remove topic
+        if self_id == head_post_id:
+            topic.delete()
+        else:
+            try:
+                topic.last_post = Post.objects.filter(topic__id=topic.id).latest()
+            except Post.DoesNotExist:
+                topic.last_post = None
+            topic.post_count = Post.objects.filter(topic__id=topic.id).count()
+            topic.save()
+        try:
+            forum.last_post = Post.objects.filter(topic__forum__id=forum.id).latest()
+        except Post.DoesNotExist:
+            forum.last_post = None
+        #TODO: for speedup - save/update only changed fields
+        forum.post_count = Post.objects.filter(topic__forum__id=forum.id).count()
+        forum.topic_count = Topic.objects.filter(forum__id=forum.id).count()
+        forum.save()
+        profile.post_count = Post.objects.filter(user__id=self.user_id).count()
+        profile.save()
+    """
+            
 
