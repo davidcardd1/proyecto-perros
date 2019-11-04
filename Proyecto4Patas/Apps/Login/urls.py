b18f18a478a06af38027257438cb9d1ec9a490d5
LissetCuevas <lisset.cuevasj@gmail.com>
2019-11-04 00:05:15 -0600

    Profile

--- Proyecto4Patas/Apps/Login/urls.py
+++ Proyecto4Patas/Apps/Login/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from Apps.Login.views import register
+from Apps.Login.views import register, profile
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     #path('',login),
     path('', LoginView.as_view(template_name='login.html'), name="login"),
     path('', LogoutView.as_view(template_name='Login/logout.html'), name="logout"),
-    path('register/', register)
+    path('register/', register, name="register"),
+    path('profile/', profile, name="profile")
 ]
--- Proyecto4Patas/Apps/Login/views.py
+++ Proyecto4Patas/Apps/Login/views.py
@@ -1,19 +1,27 @@
 from django.shortcuts import render, redirect
 #from django.http import HttpResponse
-#from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
-#from Login.forms import RegistrationForm
+from django.contrib.auth.models import User
+#from django.contrib.auth.forms import UserCreationForm
+from .forms import RegistrationForm
 # Create your views here.
 
 def register(request):
     if request.method =='POST':
-        form = UserCreationForm(request.POST)
+        form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
             msg='You have successfully Registered'
-            return render(request,'/login',{'msg':msg})
+            return render(request,'login.html',{'msg':msg})
     else:
-        form = UserCreationForm()
+        form = RegistrationForm()
     
     args = {'form':form}
     return render(request, 'reg_form.html', args)
+
+def profile(request, pk=None):
+    if pk:
+        user = User.objects.get(pk=pk)
+    else:
+        user = request.user
+    args = {'user': user}
+    return render(request, 'profile.html', args)
