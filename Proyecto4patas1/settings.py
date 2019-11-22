b65410c83c9ad64854c8338448c06bafa9a5aea0
davidcardd1 <davidcardvi@gmail.com>
2019-11-21 23:30:18 -0600

    diseno y estadisticas

--- Proyecto4patas1/settings.py
+++ Proyecto4patas1/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    'widget_tweaks',
     'forum',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
--- forum/models.py
+++ forum/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.db.models.signals import post_save
+from django.utils.text import Truncator
 from django.contrib.auth.models import User
 
 #user
@@ -32,6 +33,12 @@ class Topic(models.Model):
     def __str__(self):
         return self.name
 
+    def get_posts_count(self):
+        return Post.objects.filter(thread__topic=self).count()
+    def get_last_post(self):
+        return Post.objects.filter(thread__topic=self).order_by('-created').first()
+
+
 class Thread(models.Model):
     name = models.CharField('Name', max_length=255)
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE, default='0', related_name='threads')
@@ -41,9 +48,13 @@ class Thread(models.Model):
     body = models.TextField('Message')
     body_html = models.TextField('HTML version')
     post_count = models.IntegerField('Post count', blank=True, default=0)
-    #last_post = models.ForeignKey('Post', related_name='last_forum_post', blank=True, null=True)
+    no_views = models.IntegerField('Views count', blank=True, default=0)
+    
     def __str__(self):
         return self.name
+    
+    def get_posts_count(self):
+        return Post.objects.filter(thread=self).count()
 
 class Post(models.Model):
     thread = models.ForeignKey(Thread, on_delete=models.CASCADE, null=False, default='0', related_name='posts')
@@ -52,3 +63,6 @@ class Post(models.Model):
     updated = models.DateTimeField('Updated', auto_now=True)
     body = models.TextField('Message')
     body_html = models.TextField('HTML version')
+    def __str__(self):
+        pm = Truncator(self.body)
+        return pm.chars(30)
