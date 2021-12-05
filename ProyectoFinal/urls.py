5d7bbee32254a09a1acff67e3e17205e691007a8
davidcard1 <davidcardvi@gmail.com>
2021-12-04 23:08:06 -0600

    actualizacion de proyecto

--- ProyectoFinal/urls.py
+++ ProyectoFinal/urls.py
@@ -1,4 +1,4 @@
-"""Proyecto4patas1 URL Configuration
+"""ProyectoFinal URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
